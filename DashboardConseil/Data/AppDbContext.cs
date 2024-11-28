ab8b1123610e24a48f8dbad3f2bc971730c5977d
Your Name <you@example.com>
2024-11-28 23:12:22 +0100

    first commit

---
+++ DashboardConseil/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DashboardConseil.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DashboardConseil.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    }
+}
